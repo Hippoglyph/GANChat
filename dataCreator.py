52fb79bd999daab4b9d0064b9ea43a8eaefbc517
rinnarv <rinnarv@kth.se>
2019-04-24 17:59:55 +0200

    filtered urls and images from rawStreams

--- dataCreator.py
+++ dataCreator.py
@@ -12,23 +12,27 @@ class RawTweetExtractor():
 
 	def __init__(self):
 		self.totalTweets = 0
-		self.englishTweets = 0
+		self.filteredTweets = 0
 		self.numberOfTweetsPerFile = 100000
 		self.fileId = 0
 		self.list = []
 
 	def feed(self, tweet):
-		if "delete" not in tweet:
-			self.totalTweets += 1
+		if "delete" in tweet:
+			return
+		self.totalTweets += 1
 
-			if tweet["lang"] == "en":
-				#Filter URLs and images
-				self.englishTweets += 1
-				self.appendTweet(tweet)
-		
+		if tweet["lang"] == "en" and "retweeted_status" not in tweet:
+			if tweetContainsLinks(tweet):
+				return
+			self.filteredTweets += 1
+			self.appendTweet(tweet)
+	
 	def appendTweet(self, tweet):
 
-		self.list.append({"id": tweet["id"], "in_reply_to_status_id": tweet["in_reply_to_status_id"], "text": tweet["text"]})
+		text = tweet["extended_tweet"]["full_text"] if "extended_tweet" in tweet else tweet["text"]
+
+		self.list.append({"id": tweet["id"], "in_reply_to_status_id": tweet["in_reply_to_status_id"], "text": text})
 
 		if len(self.list) >= self.numberOfTweetsPerFile:
 			self.dumpTweets()
@@ -36,13 +40,35 @@ class RawTweetExtractor():
 	def dumpTweets(self):
 		if len(self.list) > 0:
 			#Add instance id token to name
-			with open(os.path.join(pathToEnglishFiltered, str(self.fileId) + ".json"), 'w+') as file:
+			with open(os.path.join(pathToEnglishFiltered, "A" + str(self.fileId) + ".json"), 'w+') as file:
 				for tweet in self.list:
 					json.dump(tweet, file)
 					file.write("\n")
 
 			self.fileId += 1
 			self.list = []
+
+def tweetContainsLinks(tweet):
+	if "entities" not in tweet:
+		return False
+
+	if "media" in tweet["entities"] and tweet["entities"]["media"]:
+		return True
+
+	if "urls" in tweet["entities"] and tweet["entities"]["urls"]:
+		return True
+
+	if "polls" in tweet["entities"] and tweet["entities"]["polls"]:
+		return True
+
+	if "extended_entities" in tweet and "media" in tweet["extended_entities"] and tweet["extended_entities"]["media"]:
+		return True
+
+	if "extended_tweet" in tweet:
+		return tweetContainsLinks(tweet["tweetContainsLinks"])
+
+	return False
+
 
 def extractTmpbz(rte):
 	for file in os.listdir(tempbzFolder):
@@ -69,14 +95,14 @@ def extractRawTweets():
 							theTarFile.extract(member, tempbzFolder)
 							extractTmpbz(rte)
 	rte.dumpTweets()
-	#Write to file instead
-	print("  TotalTweets:" + rte.totalTweets)
-	print("EnglishTweets:" + rte.englishTweets)
+
+	with open("RawTweetExtractorRun.txt", "w+") as file:
+		file.write("TotalTweets: " + str(rte.totalTweets))
+		file.write("FilteredTweets: " + str(rte.filteredTweets))
 
 def main():
+	rte = RawTweetExtractor()
 	extractRawTweets()
-							
-
 
 if __name__ == "__main__":
 	main()
