96d5c46198b43dc67845e56cada71397f32955c2
rinnarv <rinnarv@kth.se>
2019-04-25 19:07:47 +0200

    refactor rawTweetExtractor (untested)

--- dataCreator.py
+++ dataCreator.py
@@ -14,12 +14,16 @@ pathToError = os.path.join(pathToTweets, "Errors")
 
 class RawTweetExtractor():
 
-	def __init__(self):
+	def __init__(self, instanceID):
 		self.totalTweets = 0
 		self.filteredTweets = 0
 		self.numberOfTweetsPerFile = 100000
 		self.fileId = 0
 		self.list = []
+		self.instanceID = instanceID
+
+	def start(self)
+		self.extractRawTweets()
 
 	def feed(self, tweet):
 		if "delete" in tweet:
@@ -27,7 +31,7 @@ class RawTweetExtractor():
 		self.totalTweets += 1
 
 		if tweet["lang"] == "en" and "retweeted_status" not in tweet:
-			if tweetContainsLinks(tweet):
+			if self.tweetContainsLinks(tweet):
 				return
 			self.filteredTweets += 1
 			self.appendTweet(tweet)
@@ -43,8 +47,7 @@ class RawTweetExtractor():
 
 	def dumpTweets(self):
 		if len(self.list) > 0:
-			#Add instance id token to name
-			with open(os.path.join(pathToEnglishFiltered, "A" + str(self.fileId) + ".json"), 'w+', encoding="utf-8") as file:
+			with open(os.path.join(pathToEnglishFiltered, self.instanceID + str(self.fileId) + ".json"), 'w+', encoding="utf-8") as file:
 				for tweet in self.list:
 					json.dump(tweet, file)
 					file.write("\n")
@@ -52,73 +55,71 @@ class RawTweetExtractor():
 			self.fileId += 1
 			self.list = []
 
-def tweetContainsLinks(tweet):
-	if "entities" not in tweet:
+	def tweetContainsLinks(self, tweet):
+		if "entities" not in tweet:
+			return False
+
+		if "media" in tweet["entities"] and tweet["entities"]["media"]:
+			return True
+
+		if "urls" in tweet["entities"] and tweet["entities"]["urls"]:
+			return True
+
+		if "polls" in tweet["entities"] and tweet["entities"]["polls"]:
+			return True
+
+		if "extended_entities" in tweet and "media" in tweet["extended_entities"] and tweet["extended_entities"]["media"]:
+			return True
+
+		if "extended_tweet" in tweet:
+			return self.tweetContainsLinks(tweet["tweetContainsLinks"])
+
 		return False
 
-	if "media" in tweet["entities"] and tweet["entities"]["media"]:
-		return True
 
-	if "urls" in tweet["entities"] and tweet["entities"]["urls"]:
-		return True
-
-	if "polls" in tweet["entities"] and tweet["entities"]["polls"]:
-		return True
-
-	if "extended_entities" in tweet and "media" in tweet["extended_entities"] and tweet["extended_entities"]["media"]:
-		return True
-
-	if "extended_tweet" in tweet:
-		return tweetContainsLinks(tweet["tweetContainsLinks"])
-
-	return False
-
-
-def extractTmpbz(rte):
-	for file in os.listdir(tempbzFolder):
-		fullPath = os.path.join(tempbzFolder, file)
-		with bz2.BZ2File(fullPath, "rb") as bzFile:
-			for tweet in bzFile.readlines():
-				if tweet:
-					try:
-						rte.feed(json.loads(tweet.decode("utf-8", "ignore")))
-					except:
-						errorid = random.randrange(99999)
-						print("Could not proccess tweet ("+str(errorid)+")")
-						with open(os.path.join(pathToError, str(errorid)+ ".txt"), "w+", encoding="utf-8") as file:
-							file.write(json.loads(tweet.decode("utf-8", "ignore")))
-		os.remove(fullPath)
-
-def extractRawTweets():
-	start = time.time()
-	if not os.path.exists(pathToEnglishFiltered):
-		os.makedirs(pathToEnglishFiltered)
-	if os.path.exists(tempbzFolder):
+	def extractTmpbz(self):
 		for file in os.listdir(tempbzFolder):
-			os.remove(os.path.join(tempbzFolder, file))
-	rte = RawTweetExtractor()
-	for stream in os.listdir(pathToRaw):
-		print("Stream: " + stream)
-		pathToFile = os.path.join(pathToRaw, stream)
-		for file in os.listdir(pathToFile):
-			if file.endswith(".tar"):
-				print("------ID: " + file)
-				with tarfile.open(os.path.join(pathToFile, file)) as theTarFile:
-					for member in theTarFile.getmembers():
-						if member.path.endswith(".bz2"):
-							member.name = os.path.basename(member.name)
-							theTarFile.extract(member, tempbzFolder)
-							extractTmpbz(rte)
-	rte.dumpTweets()
+			fullPath = os.path.join(tempbzFolder, file)
+			with bz2.BZ2File(fullPath, "rb") as bzFile:
+				for tweet in bzFile.readlines():
+					if tweet:
+						try:
+							self.feed(json.loads(tweet.decode("utf-8", "ignore")))
+						except:
+							errorid = random.randrange(99999)
+							print("Could not proccess tweet ("+str(errorid)+")")
+							with open(os.path.join(pathToError, str(errorid)+ ".txt"), "w+", encoding="utf-8") as file:
+								file.write(json.loads(tweet.decode("utf-8", "ignore")))
+			os.remove(fullPath)
 
-	with open("RawTweetExtractorRun.txt", "w+") as file:
-		file.write("Time: " +str((time.time() - start)/60) + " minutes")
-		file.write("TotalTweets: " + str(rte.totalTweets))
-		file.write("FilteredTweets: " + str(rte.filteredTweets))
+	def extractRawTweets(self):
+		start = time.time()
+		if not os.path.exists(pathToEnglishFiltered):
+			os.makedirs(pathToEnglishFiltered)
+		if os.path.exists(tempbzFolder):
+			for file in os.listdir(tempbzFolder):
+				os.remove(os.path.join(tempbzFolder, file))
+		for stream in os.listdir(pathToRaw):
+			print("Stream: " + stream)
+			pathToFile = os.path.join(pathToRaw, stream)
+			for file in os.listdir(pathToFile):
+				if file.endswith(".tar"):
+					print("------ID: " + file)
+					with tarfile.open(os.path.join(pathToFile, file)) as theTarFile:
+						for member in theTarFile.getmembers():
+							if member.path.endswith(".bz2"):
+								member.name = os.path.basename(member.name)
+								theTarFile.extract(member, tempbzFolder)
+								self.extractTmpbz()
+		self.dumpTweets()
+
+		with open("RawTweetExtractorRun.txt", "w+") as file:
+			file.write("Time: " +str((time.time() - start)/60) + " minutes")
+			file.write("TotalTweets: " + str(self.totalTweets))
+			file.write("FilteredTweets: " + str(self.filteredTweets))
 
 def main():
-	rte = RawTweetExtractor()
-	extractRawTweets()
+	RawTweetExtractor("A").start()
 
 if __name__ == "__main__":
 	main()
